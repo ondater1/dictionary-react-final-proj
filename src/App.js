44330448955922798e53094f8e309b2f40b76273
ondater1 <77303472+ondater1@users.noreply.github.com>
2021-05-22 23:08:23 +0300

    Fake searchengine added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import HeaderImg from "./HeaderImg.png";
+import Dictionary from "./Dictionary";
 
 import './App.css';
+
 
 export default function App() {
   return (
     <div className="App">
       <header className="App-header">
     <img src={HeaderImg} className='App-logo' alt="logo" />
+    <h1>Dictionary</h1>
       </header>
-      <h1>Dictionary</h1>
+      <main>
+        
+        <Dictionary />
+      </main>
+
+      <footer className="App-footer">
+        <small>Coded by Alla Strakhova</small></footer>
     </div>
   );
 }
