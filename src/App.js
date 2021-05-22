2c7775f314cccbec9b7dee91d5fb686865ea8cb6
ondater1 <77303472+ondater1@users.noreply.github.com>
2021-05-22 10:47:17 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import HeaderImg from "./HeaderImg.png";
+
+import './App.css';
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+    <img src={HeaderImg} className='App-logo' alt="logo" />
+      </header>
+      <h1>Dictionary</h1>
+    </div>
+  );
+}
+
